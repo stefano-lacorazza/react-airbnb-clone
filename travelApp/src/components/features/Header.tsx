9177d29f19ef0b353303ba165bdaf826fcf8abf8
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-07-16 16:04:01 -0500

    feat: add name to header

--- travelApp/src/components/features/Header.tsx
+++ travelApp/src/components/features/Header.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import briefcaseImage from '../../assets/images/briefcase.svg';
 import userImage from '../../assets/images/user.svg';
 import { Link } from 'react-router-dom'; 
+import { useSelector } from 'react-redux';
+import { RootState } from '../../app/store';
 
 type HeaderProps = {
   logged: boolean;
+
 };
+
 
 const Header: React.FC<HeaderProps> = ({logged}) => {
   let navButtons: JSX.Element | null = null;
-
+  const name=  useSelector((state: RootState) => state.auth.currentUser?.fullName);
   if (logged){
     navButtons = <>
       <nav data-test-id='header-nav' className='header__nav'>
@@ -25,7 +29,7 @@ const Header: React.FC<HeaderProps> = ({logged}) => {
               <span className='visually-hidden'>Profile</span>
               <img src={userImage} alt='profile'></img>
               <ul data-test-id='header-profile-nav-list' className='profile-nav__list'>
-                <li data-test-id='header-profile-nav-username' className='profile-nav__item'>John Doe</li>
+                <li data-test-id='header-profile-nav-username' className='profile-nav__item'>{name}</li>
                 <li className='profile-nav__item'>
                   <button data-test-id='header-profile-nav-sign-out' className='profile-nav__sign-out button' onClick={() => { window.location.href = '/sign-in'; }}>
                     Sign Out
